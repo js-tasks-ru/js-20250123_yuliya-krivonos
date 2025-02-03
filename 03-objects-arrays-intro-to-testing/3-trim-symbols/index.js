6cf413fc49f4c73e0f0b356574a4d42196c033f8
yuliya.krivonos <Yuliya.Krivonos@south.rt.ru>
2025-02-03 12:14:28 +0300

    Задача по сортировке и trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) return '';
+  if (!Number(size)) return string;
 
+  let result = '';
+  let count = 0; // Счетчик последовательных символов
+  let previousChar = ''; // Храним предыдущий символ
+
+  for (const char of string) {
+    // Если текущий символ совпадает с предыдущим, увеличиваем счетчик
+    if (char === previousChar) {
+      count++;
+    } else {
+      count = 1; // Сбрасываем счетчик, если символ изменился
+    }
+
+    // Если счетчик не превышает size, добавляем символ в результат
+    if (count <= size) {
+      result += char;
+    }
+
+    previousChar = char; // Обновляем предыдущий символ
+  }
+
+  return result; // Возвращаем результат
 }
+
+
